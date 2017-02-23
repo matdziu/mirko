5b3dfacc88732cc12ef7e50c8595d313dab2604a
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-02-23 10:26:00 +0100

    Basic setup with colors and first screen

---
+++ app/src/main/java/pl/mirko/login/LoginActivity.java
@@ -0,0 +1,25 @@
+package pl.mirko.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import pl.mirko.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        LoginFragment loginFragment = new LoginFragment();
+        fragmentTransaction.add(R.id.login_fragment_container, loginFragment);
+        fragmentTransaction.commit();
+    }
+}
