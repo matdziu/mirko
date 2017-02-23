00ba9a205620d2bf0e30ed6b9563de019f270356
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-02-23 11:38:20 +0100

    Added basic file for sign up feature

--- app/src/main/java/pl/mirko/signup/SignUpFragment.java
+++ app/src/main/java/pl/mirko/signup/SignUpFragment.java
@@ -1,6 +1,5 @@
-package pl.mirko.login;
+package pl.mirko.signup;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,22 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import pl.mirko.R;
-import pl.mirko.signup.SignUpActivity;
 
-public class LoginFragment extends Fragment {
+public class SignUpFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         ButterKnife.bind(this, view);
         return view;
-    }
-
-    @OnClick(R.id.sign_up_button)
-    public void onSignUpButtonClicked() {
-        startActivity(new Intent(getContext(), SignUpActivity.class));
     }
 }
