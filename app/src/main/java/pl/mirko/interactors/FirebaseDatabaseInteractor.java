24446b41d45a28841e9e56c739c7d97c57ee0195
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-02-23 13:40:52 +0100

    Implemented FirebaseDatabaseInteractor

--- app/src/main/java/pl/mirko/interactors/FirebaseDatabaseInteractor.java
+++ app/src/main/java/pl/mirko/interactors/FirebaseDatabaseInteractor.java
@@ -1,12 +1,25 @@
 package pl.mirko.interactors;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import pl.mirko.interactors.interfaces.DatabaseInteractor;
 import pl.mirko.models.User;
 
 public class FirebaseDatabaseInteractor implements DatabaseInteractor {
 
+    private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+    private static final String USERS = "users";
+
     @Override
     public void createNewUser(User newUser) {
-
+        databaseReference
+                .child(USERS)
+                .child(firebaseUser.getUid())
+                .setValue(newUser);
     }
 }
