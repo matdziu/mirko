cea12fd54a90270e659c1bacf82bae80329ec29a
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-02-25 15:39:26 +0100

    Added toolbar without options menu to login activity

--- app/src/main/java/pl/mirko/login/LoginActivity.java
+++ app/src/main/java/pl/mirko/login/LoginActivity.java
@@ -4,21 +4,31 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import pl.mirko.R;
+import pl.mirko.base.BaseActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         fragmentTransaction.add(R.id.login_fragment_container, new LoginFragment());
         fragmentTransaction.commit();
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
     }
 }
