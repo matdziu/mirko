ab666f037e0b6185dde76f60afafe64e419f7b22
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-03-08 13:21:26 +0100

    Added viewHolder to custom AutoCompleteTextView adapter

--- app/src/main/java/pl/mirko/adapters/TagSuggestionsAdapter.java
+++ app/src/main/java/pl/mirko/adapters/TagSuggestionsAdapter.java
@@ -2,12 +2,20 @@ package pl.mirko.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.TextView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import pl.mirko.R;
 import pl.mirko.createpost.CreatePostPresenter;
 
 public class TagSuggestionsAdapter extends ArrayAdapter<String> implements Filterable {
@@ -15,13 +23,44 @@ public class TagSuggestionsAdapter extends ArrayAdapter<String> implements Filte
     private List<String> allTags;
     private List<String> tagSuggestions;
     private CreatePostPresenter createPostPresenter;
+    private Context context;
 
     public TagSuggestionsAdapter(List<String> initialTagSuggestions, List<String> allTags,
                                  Context context, CreatePostPresenter createPostPresenter) {
-        super(context, android.R.layout.simple_dropdown_item_1line, initialTagSuggestions);
+        super(context, R.layout.item_tag_suggestion, initialTagSuggestions);
         this.tagSuggestions = initialTagSuggestions;
         this.allTags = allTags;
         this.createPostPresenter = createPostPresenter;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context)
+                    .inflate(R.layout.item_tag_suggestion, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.tagSuggestionTextView.setText(tagSuggestions.get(position));
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+
+        @BindView(R.id.tag_suggestion_text_view)
+        TextView tagSuggestionTextView;
+
+        ViewHolder(View itemView) {
+            ButterKnife.bind(this, itemView);
+        }
     }
 
     @NonNull
