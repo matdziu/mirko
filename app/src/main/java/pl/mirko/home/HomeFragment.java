3ae4c14021e6bd5ab4e7b8460a85834536106129
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-03-17 07:58:52 +0100

    Now dismissing suggestions of search view after user chooses to search without them

--- app/src/main/java/pl/mirko/home/HomeFragment.java
+++ app/src/main/java/pl/mirko/home/HomeFragment.java
@@ -134,6 +134,7 @@ public class HomeFragment extends Fragment implements HomeView {
             public boolean onQueryTextSubmit(String query) {
                 homePresenter.queryPosts(query);
                 searchMode = true;
+                homeContentView.requestFocus();
                 return true;
             }
 
